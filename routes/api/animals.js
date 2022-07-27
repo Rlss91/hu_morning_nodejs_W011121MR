a3edaef1998ec2ed87fa8cc1542f711ee111b39d
Rlss91 <rlss91@gmail.com>
2022-07-27 10:24:23 +0300

    animals

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -10,5 +10,18 @@ router.post("/", (req, res) => {
   animalsArr = [...animalsArr, req.body];
   res.json({ msg: "ok" });
 });
+router.patch("/", (req, res) => {
+  let animal = animalsArr.find((item) => item._id === req.body._id);
+  if (animal) {
+    animal.name = req.body.name;
+    res.json({ msg: "ok" });
+  } else {
+    res.json({ msg: "not ok" });
+  }
+});
+router.delete("/", (req, res) => {
+  animalsArr = animalsArr.filter((item) => item._id !== req.body._id);
+  res.json({ msg: "ok" });
+});
 
 module.exports = router;
